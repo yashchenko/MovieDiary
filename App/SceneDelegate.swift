9c719933f7c0f527ec923c250f47e035c3e29bd2
Ivan Yashchenko <ivan.profitx@gmail.com>
2026-08-11 21:09:01 +0300

    feat: setup strict DI container and programmatic UI. Closes MAR-191

--- App/SceneDelegate.swift
+++ App/SceneDelegate.swift
@@ -10,13 +10,15 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
+    let rootVC = MovieDiaryDependencyContainer()
+    
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(frame: UIScreen.main.bounds)
         window?.windowScene = scene
-        window?.rootViewController = UINavigationController(rootViewController: TempVC())
+        window?.rootViewController = UINavigationController(rootViewController: rootVC.makeRootVC())
         window?.makeKeyAndVisible()
     }
 
