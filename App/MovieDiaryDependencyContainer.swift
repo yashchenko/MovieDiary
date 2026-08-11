3376ca437d0c1bdeb63fd96a76550fd3bd27b59c
Ivan Yashchenko <ivan.profitx@gmail.com>
2026-08-11 04:29:34 +0300

    chore: initial commit

---
+++ App/MovieDiaryDependencyContainer.swift
@@ -0,0 +1,12 @@
+//
+//  MovieDiaryDependencyContainer.swift
+//  MovieDiary
+//
+//  Created by Ivan on 11.08.2026.
+//
+
+import Foundation
+
+class MovieDiaryDependencyContainer {
+    
+}
---
+++ App/TempVc.swift
@@ -0,0 +1,18 @@
+//
+//  TempVc.swift
+//  MovieDiary
+//
+//  Created by Ivan on 11.08.2026.
+//
+
+import UIKit
+
+class TempVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .black
+    }
+    
+}
