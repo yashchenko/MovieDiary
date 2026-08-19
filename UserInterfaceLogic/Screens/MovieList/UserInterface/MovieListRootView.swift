fdda55c5e695872651a1d854a4f16e746036c78f
Ivan Yashchenko <ivan.profitx@gmail.com>
2026-08-20 01:46:28 +0300

    MAR-193: Complete MovieList screen implementation

---
+++ UserInterfaceLogic/Screens/MovieList/UserInterface/MovieListRootView.swift
@@ -0,0 +1,63 @@
+//
+//  MovieListRootView.swift
+//  MovieDiary
+//
+//  Created by Ivan on 19.08.2026.
+//
+
+import UIKit
+
+class MovieListRootView: UIView, MovieListUserInterfaceProtocol {
+    
+    private let collectionView: UICollectionView
+    private let activityIndicator = UIActivityIndicatorView(style: .large)
+    
+    override init(frame: CGRect) {
+        let layout = UICollectionViewFlowLayout()
+        self.collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupUI() {
+        addSubview(activityIndicator)
+        addSubview(collectionView)
+        
+        activityIndicator.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            
+            activityIndicator.centerXAnchor.constraint(equalTo: centerXAnchor),
+            activityIndicator.centerYAnchor.constraint(equalTo: centerYAnchor)
+            
+        ])
+        
+        
+    }
+    
+    func render(state: MovieListViewState) {
+        if state.isLoading {
+            activityIndicator.startAnimating()
+        } else {
+            
+            activityIndicator.stopAnimating()
+        }
+        
+        collectionView.isHidden = state.isLoading
+    }
+    
+  
+    
+    
+}
---
+++ UserInterfaceLogic/Screens/MovieList/UserInterface/MovieListUserInterfaceProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  MovieListUserInterfaceProtocol.swift
+//  MovieDiary
+//
+//  Created by Ivan on 19.08.2026.
+//
+
+import UIKit
+
+protocol MovieListUserInterfaceProtocol {
+    
+    func render(state: MovieListViewState)
+    
+}
