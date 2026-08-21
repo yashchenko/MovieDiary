d72e40a055639725522e08feb54fcfe7199c8141
Ivan Yashchenko <ivan.profitx@gmail.com>
2026-08-21 06:38:16 +0300

    MAR-195: Implement interaction responder and crete view controller

--- UserInterfaceLogic/Screens/MovieList/MovieListVC.swift
+++ UserInterfaceLogic/Screens/MovieList/MovieListVC.swift
@@ -5,4 +5,36 @@
 //  Created by Ivan on 19.08.2026.
 //
 
-import Foundation
+import UIKit
+
+class MovieListVC: UIViewController, MovieIxResponder {
+    
+    private let viewSome: MovieListUserInterfaceProtocol
+    private let useCase: FetchPopularMoviesProtocol
+    
+    override func loadView() {
+        
+        guard let view = viewSome as? UIView else {
+            super.loadView()
+            return
+        }
+        
+        self.view = view
+    }
+    
+    init(view: MovieListUserInterfaceProtocol, useCase: FetchPopularMoviesProtocol) {
+        self.viewSome = view
+        self.useCase = useCase
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func screenDidReady() {
+        print("view is ready")
+    }
+}
